bc16f02927cd30d7e0dbb72e91e87a1feb25bf6d
GustavoAngel2 <124698903+GustavoAngel2@users.noreply.github.com>
2024-02-07 11:19:07 -0600

    tf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table'
 
 
 @NgModule({
@@ -31,6 +32,7 @@ import { MatListModule } from '@angular/material/list';
     MatIconModule,
     MatDividerModule,
     MatListModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
